60d2743ffc87ff7bf344a236fffbd0e3d82459ac
Tricia Amheiser <tricia@taarts.net>
2022-07-26 16:42:44 -0400

    Uses Json  Deserializer > Json from API to List

--- Program.cs
+++ Program.cs
@@ -1,17 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace OneListClient
 {
     class Program
     {
+        class Item
+        {
+            public int id { get; set; }
+            public string text { get; set; }
+            public bool complete { get; set; }
+            public string created_at { get; set; }
+            public string updated_at { get; set; }
+        }
+
         static async Task Main(string[] args)
         {
             var client = new HttpClient();
 
-            var responseAsString = await client.GetStringAsync("https://one-list-api.herokuapp.com/items?access_token=sdg-handbook");
-            Console.WriteLine(responseAsString);
+            var responseAsStream = await client.GetStreamAsync("https://one-list-api.herokuapp.com/items?access_token=sdg-handbook");
+
+            var items = await JsonSerializer.DeserializeAsync<List<Item>>(responseAsStream);
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"The task {item.text}, was created on {item.created_at} and has a completion of: {item.complete}");
+            }
         }
     }
 }
