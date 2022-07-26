1e5f04e0d088ed2e8631ec4b066ec396dae73bcd
Tricia Amheiser <tricia@taarts.net>
2022-07-26 16:45:51 -0400

    time t0 datetime

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace OneListClient
             public int id { get; set; }
             public string text { get; set; }
             public bool complete { get; set; }
-            public string created_at { get; set; }
+            public DateTime created_at { get; set; }
             public string updated_at { get; set; }
         }
 
